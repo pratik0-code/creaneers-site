12cb57bb0a623fb343380d8f18bd299db9486134
pratik0-code <programmingguruji3@gmail.com>
2025-12-12 09:46:58 +0545

    smallconfiguration

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -31,7 +31,7 @@ export default function Footer() {
 
             <div className="max-w-7xl mx-auto mt-20 pt-8 border-t border-neutral-800 dark:border-neutral-800 flex justify-between items-center text-xs text-neutral-500 dark:text-neutral-400 uppercase tracking-wider">
                 <span>© {new Date().getFullYear()} CREANEERS Architecture.</span>
-                <span>Made with Next.js</span>
+                <span>Made by Pratik Fuyal</span>
             </div>
         </footer>
     );
