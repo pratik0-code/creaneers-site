28440d3726162fabb3505f84ff88101be274ccc6
pratik0-code <programmingguruji3@gmail.com>
2026-03-22 10:54:51 +0545

    footerupdated2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,7 +36,7 @@ export default function Footer() {
 
             <div className="max-w-7xl mx-auto mt-20 pt-8 border-t border-neutral-800 flex justify-between items-center text-xs text-neutral-500 dark:text-neutral-400 uppercase tracking-wider">
                 <span>© {new Date().getFullYear()} CREANEERS Design and Consult.</span>
-                <span className="text-[10px] text-neutral-600 text-right leading-tight opacity-80">Made by pratik0-code <br /></span>
+                <span className="text-[10px] text-neutral-600 text-right leading-tight opacity-80">Made by pratik0-code</span>
             </div>
         </footer >
     );
